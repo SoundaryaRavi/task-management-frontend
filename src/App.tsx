38fca31eb14dd6fd3202e486adb1cfe2d24c754e
Soundarya <soundarya.r@uitoux.in>
2025-04-30 19:32:07 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+// import Routes from "./routes/Routes";
+
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      {/* <Routes /> */}
+    </BrowserRouter>
+  );
+};
+
+export default App;
